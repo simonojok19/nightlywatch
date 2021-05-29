aaf714ddbbb4ecc353d762046939757ef2237929
simonojok19 <simonojok19@gmail.com>
2021-05-29 23:53:48 +0300

    Initial Commit

---
+++ NightWatch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NightWatch
+//
+//  Created by McKeyo on 29/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NightWatch/NightWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightWatchApp.swift
+//  NightWatch
+//
+//  Created by McKeyo on 29/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct NightWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
