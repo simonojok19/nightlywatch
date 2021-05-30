f5b895d6cda00611626d1f6981b61ad812055b71
simonojok19 <simonojok19@gmail.com>
2021-05-30 08:43:50 +0300

    learned about HStack, VStack, ZStack

--- NightWatch/ContentView.swift
+++ NightWatch/ContentView.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Nighty Tasks")
-        Text("Weekly Tasks")
-        Text("Monthly Tasks")
+        VStack {
+            Text("Nighty Tasks")
+            Text("Weekly Tasks")
+            Text("Monthly Tasks")
+        }
     }
 }
 
